b3c1172c9a8c4a0e53b21c7989049d1d485e06f3
LordHunk <sofiane.fatnassi@gmail.com>
2021-02-21 17:39:22 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# draggable-part-5-jo5g5r
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/draggable-part-5-jo5g5r)
---
+++ app/app.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+
+import { SortEvent } from './draggable/sortable-list.directive';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+
+  sortableList = [
+    'Box 1',
+    'Box 2',
+    'Box 3',
+    'Box 4',
+    'Box 5',
+    'Box 6',
+    'Box 7',
+    'Box 8',
+    'Box 9',
+    'Box 10'
+  ];
+
+  sort(event: SortEvent) {
+    const current = this.sortableList[event.currentIndex];
+    const swapWith = this.sortableList[event.newIndex];
+
+    this.sortableList[event.newIndex] = current;
+    this.sortableList[event.currentIndex] = swapWith;
+  }
+}
